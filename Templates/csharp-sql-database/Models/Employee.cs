da8f5f4c16fffaab773579c3a6a953b29802839a
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-13 21:15:31 +0430

    Implements csharp-sql-database

---
+++ Templates/csharp-sql-database/Models/Employee.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsoleApp.Models; 
+
+public class Employee {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Salary { get; set; }
+    public int? ManagerId { get; set; }
+}
