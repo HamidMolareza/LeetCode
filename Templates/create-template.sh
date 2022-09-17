de79a3dffc2ef3943a00b132306821b0e924c93c
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-18 02:45:02 +0430

    Updates create-template.sh

--- Templates/create-template.sh
+++ Templates/create-template.sh
@@ -19,13 +19,34 @@ create_dir_if_is_not_exist() {
     mkdir "$1"
   fi
 }
+
+ensure_problem_name_is_valid() {
+  problem_name="$1"
+  
+  printf "Validating problem name... "
+  status_code=$(curl -s -o /dev/null -w "%{http_code}" https://leetcode.com/problems/$problem_name/)
+  if [ "$status_code" != "200" ]; then
+    echo "Error! It seems that the name is not valid. (Id: $problem_name, status code: $status_code)"
+    exit 1
+  fi
+  echo "done."
+}
+
+ensure_ide_is_valid() {
+  ide="$1"
+  if ! command -v "$ide" &>/dev/null; then
+    echo "the ide command could not be found. (Ide: $ide)"
+    exit 1
+  fi
+}
 #===========================================================
 # Get Inputs
-problemName="$1"
-if [ -z "$problemName" ]; then
+problem_name="$1"
+if [ -z "$problem_name" ]; then
   printf "Problem name (slug): "
-  read -r problemName
+  read -r problem_name
 fi
+ensure_problem_name_is_valid "$problem_name"
 
 templateDir="$2"
 if [ -z "$templateDir" ]; then
@@ -43,42 +64,44 @@ text_editor="$3"
 if [ -z "$text_editor" ]; then
   text_editor="code"
 fi
+ensure_ide_is_valid "$text_editor"
 
 ide="$4"
 if [ -z "$ide" ]; then
   printf "Ide: "
   read -r ide
 fi
+ensure_ide_is_valid "$ide"
 
-solutionsDir="$5"
-if [ -z "$solutionsDir" ]; then
-  solutionsDir="../Solutions"
-  if [ ! -d "$solutionsDir" ]; then
+solutions_dir="$5"
+if [ -z "$solutions_dir" ]; then
+  solutions_dir="../Solutions"
+  if [ ! -d "$solutions_dir" ]; then
     printf "Solutions directory: "
-    read -r solutionsDir
+    read -r solutions_dir
   fi
 fi
-if [ ! -d "$solutionsDir" ]; then
-  echo "Error! Can not find solutions dir in $solutionsDir"
+if [ ! -d "$solutions_dir" ]; then
+  echo "Error! Can not find solutions dir in $solutions_dir"
   exit 1
 fi
 #===========================================================
 
 #checkout
-git checkout -b "$problemName"
-exit_if_operation_failed "$?" "Can not checkout to $problemName"
+git checkout -b "$problem_name"
+exit_if_operation_failed "$?" "Can not checkout to $problem_name"
 
 #create solution dir
-create_dir_if_is_not_exist "$solutionsDir/$problemName"
+create_dir_if_is_not_exist "$solutions_dir/$problem_name"
 
 #copy template to solution dir
-resultDir="$solutionsDir/$problemName"
-cp -r "$templateDir" "$resultDir"
-exit_if_operation_failed "$?" "Can not copy template from $templateDir to $resultDir"
+result_dir="$solutions_dir/$problem_name"
+cp -r "$templateDir" "$result_dir"
+exit_if_operation_failed "$?" "Can not copy template from $templateDir to $result_dir"
 wait
 
 #Create README file for question text
-questionText="$resultDir/README.md"
+questionText="$result_dir/README.md"
 if [ ! -f "$questionText" ]; then
   echo 'Copy the question text here.' >"$questionText"
   echo "Please copy the question text to $questionText"
@@ -87,13 +110,13 @@ if [ ! -f "$questionText" ]; then
   warning_if_operation_failed "$?" "Can not open your text editor for $questionText"
 fi
 
-echo "Directory is ready: $resultDir"
-$ide "$resultDir" >/dev/null
-warning_if_operation_failed "$?" "Can not open your ide for $resultDir"
+echo "Directory is ready: $result_dir"
+$ide "$result_dir" >/dev/null
+warning_if_operation_failed "$?" "Can not open your ide for $result_dir"
 
 echo ""
 printf "Do you want merge this branch to master branch?(y/N) "
 read -r merge_confirm
 if [ "$merge_confirm" = 'y' ] || [ "$merge_confirm" = 'Y' ]; then
-  ./merge-into-master-branch.sh "$problemName" "y"
+  ./merge-into-master-branch.sh "$problem_name" "y"
 fi
