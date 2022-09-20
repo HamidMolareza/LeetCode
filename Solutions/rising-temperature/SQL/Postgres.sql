82ac5db55bb5bbf9e2bac70c99c94e034fb155ea
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-20 05:17:39 +0430

    Solves rising-temperature

---
+++ Solutions/rising-temperature/SQL/Postgres.sql
@@ -0,0 +1,40 @@
+-- Create Items
+Create table If Not Exists Weather
+(
+    id
+        int,
+    recordDate
+        date,
+    temperature
+        int
+);
+Truncate table Weather;
+insert into Weather
+(id,
+ recordDate,
+ temperature)
+values ('1',
+        '2015-01-01',
+        '10')
+        ,
+       ('2',
+        '2015-01-02',
+        '25')
+        ,
+       ('3',
+        '2015-01-03',
+        '20')
+        ,
+       ('4',
+        '2015-01-04',
+        '30');
+
+-- Write your Postgres query statement below
+SELECT w.id AS Id
+FROM Weather
+         JOIN Weather w
+              ON (w.recordDate - Weather.recordDate) = 1
+                  AND w.Temperature > Weather.Temperature;
+
+-- Remove Data
+DROP TABLE Weather;
