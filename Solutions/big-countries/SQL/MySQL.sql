a3e7c19ff73cebd612cc43f77a8986ce9a4e5bdc
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-27 20:46:44 +0330

    Solves big-countries with sql

---
+++ Solutions/big-countries/SQL/MySQL.sql
@@ -0,0 +1,24 @@
+-- Create Items
+Create table If Not Exists World (
+    name varchar(255),
+    continent varchar(255),
+    area int,
+    population int,
+    gdp int
+    );
+    Truncate table World;
+insert into World (name, continent, area, population, gdp)
+values ('Afghanistan', 'Asia', '652230', '25500100', '20343000000')
+     , ('Albania', 'Europe', '28748', '2831741', '12960000000')
+     , ('Algeria', 'Africa', '2381741', '37100000', '188681000000')
+     , ('Andorra', 'Europe', '468', '78115', '3712000000')
+     , ('Angola', 'Africa', '1246700', '20609294', '100990000000');
+
+-- Write your MySQL query statement below
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000
+   OR population >= 25000000
+
+-- Remove Data
+DROP TABLE World;
