980ca69720063f822c0dcc0a5db4e414e54b89aa
Hamid Molareza <kitm.hamid@gmail.com>
2023-01-07 02:48:22 +0330

    Updates csharp solution

---
+++ Templates/csharp/ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿var solution = new Solution();
+solution.SayHello("Hamid");
+
+
+public class Solution {
+    public void SayHello(string name) {
+        Console.WriteLine($"Hello {name}!");
+    }
+}
--- Templates/csharp/Solution/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Solution;
-
-public class Program {
-}
