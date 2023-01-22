c3c58a67c3ecb054422b3f238b1fc171e41c13bf
Hamid Molareza <kitm.hamid@gmail.com>
2023-01-22 05:26:04 +0330

    solve(search-insert-position): csharp

---
+++ Solutions/search-insert-position/csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.SearchInsert(new int[] {1, 3, 5, 6}, 5));
+Console.WriteLine(solution.SearchInsert(new int[] {1, 3, 5, 6}, 2));
+Console.WriteLine(solution.SearchInsert(new int[] {1, 3, 5, 6}, 7));
+
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        var findIndex = nums.ToList().FindIndex(num => num >= target);
+        return findIndex >= 0 ? findIndex : nums.Length;
+    }
+}
