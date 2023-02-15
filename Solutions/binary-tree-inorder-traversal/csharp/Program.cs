2deae24b65d88f918594d1cdaafb136183afb28d
Hamid Molareza <kitm.hamid@gmail.com>
2023-02-15 09:58:23 +0330

    solve(binary-tree-inorder-traversal): csharp

---
+++ Solutions/binary-tree-inorder-traversal/csharp/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Solution {
+    public IList<int> Solution1(TreeNode root) {
+        var result = new List<int>();
+        if (root is null) return result;
+        result.AddRange(Solution1(root.left));
+        result.Add(root.val);
+        result.AddRange(Solution1(root.right));
+        return result;
+    }
+
+    public IList<int> Solotion2(TreeNode root) {
+        List<int> result = new List<int>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (stack.Count > 0 || root != null) {
+            while (root != null) {
+                stack.Push(root);
+
+                root = root.left;
+            }
+
+            root = stack.Pop();
+            result.Add(root.val);
+            root = root.right;
+        }
+
+        return result;
+    }
+}
+
+public class TreeNode {
+    public int val;
+    public TreeNode? left;
+    public TreeNode? right;
+
+    public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
