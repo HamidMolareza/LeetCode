6f850d12615c8df49337250cc2c1fef976cefc1a
Hamid Molareza <kitm.hamid@gmail.com>
2023-03-16 13:12:08 +0330

    feat(find-the-index-of-the-first-occurrence-in-a-string): csharp

---
+++ Solutions/find-the-index-of-the-first-occurrence-in-a-string/csharp/Program.cs
@@ -0,0 +1,9 @@
+﻿var solution = new Solution();
+solution.StrStr("sadbutsad", "sad");
+
+
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        return haystack.IndexOf(needle, StringComparison.OrdinalIgnoreCase);
+    }
+}
