4d77308d157d6d69dab4a15bb0a88224237abebb
HamidMolareza <molareza@protonmail.com>
2024-10-31 23:09:21 +0330

    fix(solutions): remove old sql solutions

--- Solutions/classes-more-than-5-students/csharp-SQL/Models/Course.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace ConsoleApp.Models;
-
-public class Course {
-    [Key] public string Student { get; set; } = null!;
-    public string Class { get; set; } = null!;
-}
